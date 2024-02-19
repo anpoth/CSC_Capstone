f370e69c358b7898e831347af413e718946b19f4
anpoth <anpoth87@gmail.com>
2024-02-19 09:15:43 -0500

    first commit

---
+++ sampleCode/Week1.js
@@ -0,0 +1,6 @@
+function ShowHelloMessage() {
+    var name = document.getElementById("myname");
+    document.getElementById("hellomessage").innerHTML = "Hello, " + name.value;
+}
+
+document.getElementById("mybutton").onclick = ShowHelloMessage;
