cb078a355cabec82eb3b35444055e7da3b3518d8
anpoth <anpoth87@gmail.com>
2024-02-20 11:02:57 -0500

    Added functionality for the grade level navigation.

--- mainWebsite/app.js
+++ mainWebsite/app.js
@@ -7,9 +7,28 @@
 
 // #region
 // Submit Contact Request Function
-document.getElementById("submitBtn").onclick = ShowHelloMessage;
-function ShowHelloMessage() {
+document.getElementById("submitBtn").onclick = submitContactRequest;
+function submitContactRequest() {
     alert("Submitted!, You will hear from us via email shortly!");
     location.reload();
 }
 // #endregion
+
+
+
+document.getElementById("gradeSchool").onclick = showElements;
+document.getElementById("middleHigh").onclick = showElements;
+document.getElementById("college").onclick = showElements;
+document.getElementById("byCourse").onclick = showElements;
+
+
+
+// Show current element of grade level
+function showElements(elementToShow){
+    var x = document.getElementById(elementToShow);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
